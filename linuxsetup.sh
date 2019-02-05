9edab95e089ea4cfbeba82a52f9d286678a1e3b0
phdumaresq <phdumaresq@gmail.com>
2019-02-05 12:24:55 -0500

    modified linux setup for omf

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -1,7 +1,7 @@
 # install important packages
 # sudo pacman -Sy firefox
 # sudo pacman -Sy code
-sudo pacman -Sy fish
+sudo pacman -Sy fish --noconfirm
 
 
 # install powerline
@@ -9,4 +9,8 @@ git clone https://github.com/powerline/fonts.git --depth=1
 ./fonts/install.sh
 rm -rf fonts
 
-curl -L https://get.oh-my.fish | fish -c exit
+# install omf
+curl -L https://get.oh-my.fish > install
+fish install --path=~/.local/share/omf --config=~/.config/omf
+
+

7668869e8ac5a22945c2d6a9f59f92c2985805af
Philip <phdumaresq@gmail.com>
2019-02-06 20:12:58 +0000

    Fixed the omf install to not change shells

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -8,14 +8,10 @@ sudo pacman -Sy libc++ --noconfirm
 git config --global user.email = "phdumaresq@gmail.com"
 git config --global user.name = "Philip"
 
-# install powerline
+# install powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
 ./fonts/install.sh
 rm -rf fonts
-
-# install omf
-curl -L https://get.oh-my.fish > install
-fish install --path=~/.local/share/omf --config=~/.config/omf
 
 #intall discord
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/discord.tar.gz
@@ -23,4 +19,9 @@ tar -xvf discord*.tar.gz
 cd discord
 makepkg -sri --noconfirm
 
+#install omf
+curl -L https://get.oh-my.fish >  oh-my.fish
+fish -c "chmod +x ./oh-my.fish; ./oh-my.fish --noninteractive -y"
 
+#install fish themes
+fish -c "omf install agnoster"

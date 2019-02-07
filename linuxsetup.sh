2103013bddb2d28730f3f6d055259d8bb635a497
= <=>
2019-02-07 00:08:43 +0000

    demo

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -3,7 +3,7 @@
 # install important packages
 # sudo pacman -Sy firefox --noconfirm
 # sudo pacman -Sy code --noconfirm
-# sudo pacman -Sy fish --noconfirm
+sudo pacman -Sy fish --noconfirm
 # sudo pacman -Sy python python-pip --noconfirm
 # sudo pacman -Sy libc++ --noconfirm
 # sudo pacman -Sy nodejs npm --noconfirm
@@ -14,9 +14,9 @@ git config --global user.email = "phdumaresq@gmail.com"
 git config --global user.name = "Philip"
 
 # install powerline fonts
-# git clone https://github.com/powerline/fonts.git --depth=1
-# ./fonts/install.sh
-# rm -rf fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+./fonts/install.sh
+rm -rf fonts
 
 #intall discord
 # wget https://aur.archlinux.org/cgit/aur.git/snapshot/discord.tar.gz
@@ -25,8 +25,8 @@ git config --global user.name = "Philip"
 # makepkg -sri --noconfirm
 
 #install omf
-# curl -L https://get.oh-my.fish >  oh-my.fish
-# fish -c "chmod +x ./oh-my.fish; ./oh-my.fish --noninteractive -y"
+curl -L https://get.oh-my.fish >  oh-my.fish
+fish -c "chmod +x ./oh-my.fish; ./oh-my.fish --noninteractive -y"
 
 #install fish themes
-# fish -c "omf install agnoster"
+fish -c "omf install agnoster"

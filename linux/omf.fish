4b5ea93993785352ad880eb91efd76f06648ce15
= <=>
2019-02-07 15:39:18 +0000

    getting powerline fonts from pacman

--- linux/omf.fish
+++ linux/omf.fish
@@ -49,3 +49,9 @@ end
 function rs
     . ~/.config/fish/conf.d/omf.fish
 end
+
+powerline-daemon -q
+set POWERLINE_BASH_CONTINUATION 1
+set POWERLINE_BASH_SELECT 1
+set fish_function_path $fish_function_path "/usr/lib/python3.7/site-packages/powerline/bindings/fish"
+powerline-setup
--- linux/setup.sh
+++ linux/setup.sh
@@ -3,7 +3,7 @@
 # install important packages
 # sudo pacman -Sy firefox --noconfirm
 # sudo pacman -Sy code --noconfirm
-sudo pacman -Sy fish --noconfirm
+# sudo pacman -Sy fish --noconfirm
 # sudo pacman -Sy python python-pip --noconfirm
 # sudo pacman -Sy libc++ --noconfirm
 # sudo pacman -Sy nodejs npm --noconfirm
@@ -14,9 +14,7 @@ git config --global user.email = "phdumaresq@gmail.com"
 git config --global user.name = "Philip"
 
 # install powerline fonts
-git clone https://github.com/powerline/fonts.git --depth=1
-./fonts/install.sh
-rm -rf fonts
+sudo pacman -Sy powerline powerline-fonts
 
 #intall discord
 # wget https://aur.archlinux.org/cgit/aur.git/snapshot/discord.tar.gz
@@ -25,10 +23,10 @@ rm -rf fonts
 # makepkg -sri --noconfirm
 
 #install omf
-curl -L https://get.oh-my.fish >  oh-my.fish
-fish -c "chmod +x ./oh-my.fish; ./oh-my.fish --noninteractive -y"
+# curl -L https://get.oh-my.fish >  oh-my.fish
+# fish -c "chmod +x ./oh-my.fish; ./oh-my.fish --noninteractive -y"
 
 #install fish themes
-fish -c "omf install agnoster"
+# fish -c "omf install agnoster; omf install spacefish"
 
-rm -rf ~/Documents/ ~/Downloads/ ~/Music/ ~/Pictures/ ~/Public/ ~/Templates/ ~/Videos/
+# rm -rf ~/Documents/ ~/Downloads/ ~/Music/ ~/Pictures/ ~/Public/ ~/Templates/ ~/Videos/

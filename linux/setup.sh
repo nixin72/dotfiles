d0286a1c0d1e3739ee882cebffd73e66d3f047da
= <=>
2019-02-13 15:24:58 -0500

    Added Spacevim to install instead of code

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,27 +1,30 @@
+#!/bin/bash
 ################################################################################
 ####################               INSTALLATION             ####################
 ################################################################################
 
 sudo pacman -Sy firefox --noconfirm
-# sudo pacman -Sy code --noconfirm
 sudo pacman -Sy fish --noconfirm
-sudo pacman -Sy python-pywal --noconfirm 
+sudo pacman -Sy python-pywal --noconfirm
 sudo pacman -Sy python-pip --noconfirm
 sudo pacman -Sy libc++ --noconfirm
 sudo pacman -Sy terminology --noconfirm
 sudo pacman -Sy neofetch --noconfirm
-# sudo pacman -Sy nodejs npm --noconfirm
-# sudo pacman -Sy sbcl --noconfirm
-# sudo pacman -Sy ruby --noconfirm
-# sudo pacman -Sy powerline powerline-fonts --noconfirm
+sudo pacman -Sy nodejs npm --noconfirm
+sudo pacman -Sy sbcl --noconfirm
+sudo pacman -Sy ruby --noconfirm
+sudo pacman -Sy powerline powerline-fonts --noconfirm
 
-#install discord
-# wget https://aur.archlinux.org/cgit/aur.git/snapshot/discord.tar.gz
-# tar -xvf discord*.tar.gz
-# cd discord
-# makepkg -sri --noconfirm
+# Install discord
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/discord.tar.gz
+tar -xvf discord*.tar.gz
+cd discord
+makepkg -sri --noconfirm
 
-#install omf and themes - install script already in the git repo
+# Install SpaceVim
+curl -sLf https://spacevim.org/install.sh | bash
+
+# Install omf and themes - install script already in the git repo
 fish -c "chmod +x ~/configs/oh-my.fish; ~/configs/oh-my.fish --noninteractive -y"
 fish -c "omf install spacefish; omf install agnoster";
 
@@ -31,45 +34,8 @@ fish -c "omf install spacefish; omf install agnoster";
 sudo pacman -Rcns subversion --noconfirm
 sudo pacman -Rcns gimp --noconfirm
 sudo pacman -Rcns palemoon --noconfirm
-# sudo pacman -Rcns zsh --noconfirm
-# sudo pacman -Rcns vlc --noconfirm
-# sudo pacman -Rcns aalib --noconfirm
-# sudo pacman -Rcns acpi --noconfirm
-# sudo pacman -Rcns acpid --noconfirm
-# sudo pacman -Rcns amd-ucode --noconfirm
-# sudo pacman -Rcns arandr --noconfirm
-# sudo pacman -Rcns apr --noconfirm
-# sudo pacman -Rcns artwork-i3 --noconfirm
-# sudo pacman -Rcns atool --noconfirm
-# sudo pacman -Rcns accountsservice --noconfirm
-# sudo pacman -Rcn  adapta-maia-theme --noconfirm
-# sudo pacman -Rcns b43-fwcutter --noconfirm
-# sudo pacman -Rcn  babl --noconfirm
-# sudo pacman -Rcns bison --noconfirm
-# sudo pacman -Rcns blueman --noconfirm
-# sudo pacman -Rcns c-ares --noconfirm
-# sudo pacman -Rcns calamares --noconfirm
-# sudo pacman -Rcns celt --noconfirm
-# sudo pacman -Rcns clonezilla --noconfirm
-# sudo pacman -Rcns chromaprint --noconfirm
-# sudo pacman -Rcns deluge --noconfirm
-# sudo pacman -Rcns dhclient --noconfirm
-# sudo pacman -Rcns dunst --noconfirm
-# sudo pacman -Rcns dosfstools --noconfirm
-# sudo pacman -Rcns dmraid --noconfirm
-# sudo pacman -Rcns downgrade --noconfirm
-# sudo pacman -Rcns dmidecode --noconfirm
-# sudo pacman -Rcns desktop-file-utils --noconfirm
-# sudo pacman -Rcns dmenu-manjaro --noconfirm
-
-
-################################################################################
-####################      REINSTALL LOST DEPENDENCIES       ####################
-################################################################################
-# sudo pacman -Sy xorg-xrand --noconfirm
-
-
-
+sudo pacman -Rcns zsh --noconfirm
+sudo pacman -Rcns vlc --noconfirm
 
 ################################################################################
 ####################              CONFIGURATION             ####################
@@ -80,7 +46,7 @@ sudo cp ~/configs/background.png /usr/share/backgrounds/loz.png
 sudo wal -i /usr/share/backgrounds/loz.png
 
 # Replace default i3 config with my own
-yes | cp ./config.i3 ~/.i3/config 
+yes | cp ./config.i3 ~/.i3/config
 
 # Clean up some of the default directories
 rm -rf ~/Documents/ ~/Music/ ~/Pictures/ ~/Public/ ~/Templates/ ~/Videos/
@@ -118,3 +84,4 @@ echo "NOTE: MAKE SURE TO CHANGE DATE AND TIME SETTINGS"
 echo "    $ sudo date +%Y-%m-%d -s \"YYYY-mm-dd\""
 echo "    $ sudo date +%T -s \"hr:min:sec\""
 
+sudo cp ./FiraCode-Retina.otf /usr/share/terminology/fonts/FiraCode-Retina.otf

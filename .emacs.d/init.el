c2f3ad6c9a0f9ce29e9c4f5171c2adc1bb4e4579
phdumaresq <phdumaresq@gmail.com>
2019-07-30 00:14:18 -0700

    Moved a bunch of stuff going on in here to external files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,20 +3,20 @@
 ;;;
 ;;; This is my personal emacs configs. I'm pretty new to emacs, so this is all new to me.
 
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  (package-refresh-contents))
-
 (load-file "~/.emacs.d/lib/functions.el")
+(start-package-manager)
+(load-paths)
 
-(import-packages
- "evil" 'evil)
+
+(require 'my-evil)
+(require 'my-defaults)
+(require 'my-general)
+(require 'my-sidebar)
+(require 'my-which-key)
+(require 'my-themes)
 
 (set-font "Fira Code")
-(set-theme 'nord)
-(evil-mode 1)
+
 
 
 ;; Disable menu bar at the top of the window
@@ -24,3 +24,17 @@
     (tool-bar-mode 0))
 
 (add-to-list 'load-path "~/.local/share/icons-in-terminal/") ;; If it's not already done
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(doom-themes evil use-package undo-tree js2-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

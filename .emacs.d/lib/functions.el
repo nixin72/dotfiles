aeeb75092941a55fc5254ca56488577dfc6a72a8
phdumaresq <phdumaresq@gmail.com>
2019-07-30 00:21:06 -0700

    Moved package management and load path logic out of init.el

--- .emacs.d/lib/functions.el
+++ .emacs.d/lib/functions.el
@@ -1,3 +1,22 @@
+(defun start-package-manager ()
+  (require 'package)
+  (setq load-prefer-newer t)
+
+  (setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+                           ("gnu"       . "http://elpa.gnu.org/packages/")
+                           ("melpa"     . "https://melpa.org/packages/")))
+  (package-initialize)
+  (unless (package-installed-p 'use-package) ; unless it is already installed
+    (package-refresh-contents) ; updage packages archive
+    (package-install 'use-package)) ; and install the most recent version of use-package
+
+  (require 'use-package))
+
+(defun load-paths ()
+  (add-to-list 'load-path (concat user-emacs-directory "config"))
+  (add-to-list 'load-path (concat user-emacs-directory "config" "/languages"))
+  (add-to-list 'load-path (concat user-emacs-directory "packages")))
+
 (defun import-packages (&rest packages)
   "Loads a package from the packages directory and imports it"
   (while packages
@@ -21,4 +40,3 @@
   "Set all keybindings in here"
   (global-set-key (kbd "C-x C-f") 'sidebar-open)
   (global-set-key (kbd "C-x C-a") 'sidebar-buffers-open))
-

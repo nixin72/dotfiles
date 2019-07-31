634ca5a48b2b906412bdc240fdbe57a1306b6189
phdumaresq <phdumaresq@gmail.com>
2019-07-30 23:46:01 -0700

    Added sidebar styles. Currently broken...

--- .emacs.d/config/my-sidebar.el
+++ .emacs.d/config/my-sidebar.el
@@ -1,7 +1,8 @@
 (use-package neotree
-	     :ensure t)
-;	     :config 
-;	     (progn
-;	       (setq neo-theme (if (display-graphic-p) 'icons 'arrow))))
+  :ensure t
+  :config
+  (progn
+    (setq neo-theme
+      (if (display-graphic-p) 'icons 'arrow))))
 
 (provide 'my-sidebar)

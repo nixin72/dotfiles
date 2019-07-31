228890d76d80336389e4b0d3befc025886058771
phdumaresq <phdumaresq@gmail.com>
2019-07-30 23:43:10 -0700

    Keybinding for <LEADER> as SPC. Doesn't work now

--- .emacs.d/config/my-general.el
+++ .emacs.d/config/my-general.el
@@ -1,4 +1,13 @@
 (use-package general
-  :ensure t)
+  :ensure t
+  :config
+  (progn
+    (general-create-definer my-leader-def
+      :prefix "SPC")
+    (my-leader-def
+      :states 'normal
+      :keymaps 'normal
+      "f t" 'neotree-toggle
+    )))
 
 (provide 'my-general)

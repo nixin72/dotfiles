002b270d90dbce87428c38e52349fcb06d4bcaef
phdumaresq <phdumaresq@gmail.com>
2019-07-30 23:46:40 -0700

    Config for whichkey

--- .emacs.d/config/my-which-key.el
+++ .emacs.d/config/my-which-key.el
@@ -1,3 +1,15 @@
-(use-package which-key :ensure t)
+(use-package which-key
+  :ensure t
+  :init
+  (progn
+    (setq which-key-separator " ")
+    (setq which-key-prefix-prefix "+"))
+  :config
+  (progn
+    (which-key-setup-side-window-right-bottom)
+    (setq which-key-sort-order 'which-key-key-order-alpha
+      which-key-side-window-max-width 0.33
+      which-key-idle-delay 0.05))
+  :diminish (which-key-mode))
 
 (provide 'my-which-key)

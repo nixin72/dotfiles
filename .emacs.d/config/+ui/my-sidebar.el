0cfa4562f2fa34d4dc574479482c74a9b82e9a00
phdumaresq <phdumaresq@gmail.com>
2019-08-03 23:40:29 -0700

    Added sidebar to UI dir. renamed startup file

--- .emacs.d/config/+ui/my-sidebar.el
+++ .emacs.d/config/+ui/my-sidebar.el
@@ -1,12 +1,15 @@
+(use-package all-the-icons
+  :ensure t)
+
 (use-package neotree
   :ensure t
   :config
   (progn
     (setq-default neo-show-hidden-files t)
     (setq-default neo-smart-open t)
+    (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+    (setq neo-show-updir-line nil)
   ))
-  ;  (setq neo-theme
-  ;    (if (display-graphic-p) 'icons 'arrow))))
 
 (neotree-dir "~/repos")
 

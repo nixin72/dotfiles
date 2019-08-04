98dc86d1eee3875651eb6c32e5b1a5027399c014
phdumaresq <phdumaresq@gmail.com>
2019-08-03 23:46:37 -0700

    Moved slack and wakatime to new dir

---
+++ .emacs.d/config/+integrations/my-wakatime.el
@@ -0,0 +1,13 @@
+(require 'my-secrets)
+
+(use-package wakatime-mode
+  :ensure t
+  :config
+  (progn
+    (setq wakatime-python-bin "/usr/local/bin/python3")
+    (setq wakatime-api-key my-wakatime-api-key)
+    (setq wakatime-cli-path "/usr/local/bin/wakatime")))
+
+(global-wakatime-mode)
+
+(provide 'my-wakatime)
